513e6cfe4e3aa1a5712ce9291a96bad58088eb71
aichaoguy <aichaoguy@live.com>
2015-05-08 20:12:42 +0800

    pep8 format

--- ipgeo/ipgeo.py
+++ ipgeo/ipgeo.py
@@ -4,8 +4,11 @@ except ImportError:
     from urllib2 import urlopen
 
 import codecs
-reader = codecs.getreader("utf-8")
 import json
+
+reader = codecs.getreader("utf-8")
+
+
 class GeoInfo:
     def __init__(self, _country, _region, _city, _isp):
         self.country = _country
@@ -16,14 +19,15 @@ class GeoInfo:
     def full_name(self):
         return "".join([self.country, self.region, self.city])
 
+
 def query(ip):
     response = urlopen('http://ip.taobao.com/service/getIpInfo.php?ip='+ip)
     data = json.load(reader(response))
     if data['code'] == 0:
         country = data['data']['country']
-        region  = data['data']['region']
-        city    = data['data']['city']
-        isp     = data['data']['isp']
+        region = data['data']['region']
+        city = data['data']['city']
+        isp = data['data']['isp']
         info = GeoInfo(country, region, city, isp)
         return info
     else:
