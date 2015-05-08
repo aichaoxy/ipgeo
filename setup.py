d426211d7743083880ef2a5fe4087379ffa0bd83
aichaoguy <aichaoguy@live.com>
2015-05-08 20:36:22 +0800

    0.2.3

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ from distutils.core import setup
 setup(
   name = 'ipgeo',
   packages = ['ipgeo'],
-  version = '0.2.2',
+  version = '0.2.3',
   description = 'Geo info retriver for ipv4 address using chinese taobao service',
   author = 'Ai Chao',
   author_email = 'aichaoguy@live.com',
   url = 'https://github.com/aichaoguy/ipgeo',
-  download_url = 'https://github.com/aichaoguy/ipgeo/tarball/0.2.2',
-  keywords = ['ip', 'geo', 'taobao'], # arbitrary keywords
+  download_url = 'https://github.com/aichaoguy/ipgeo/tarball/0.2.3',
+  keywords = ['ip', 'geo', 'taobao'],
   classifiers = [
       "Programming Language :: Python",
+      "Programming Language :: Python :: 2.6",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3",
       "Environment :: Other Environment",
       "Operating System :: OS Independent",
       "Topic :: Software Development :: Libraries :: Python Modules",
